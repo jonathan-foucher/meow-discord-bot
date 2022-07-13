f436f4cab440896fe11935c8141f74ef5b40643e
Jonathan Foucher <jonathan-foucher@outlook.fr>
2022-07-13 17:49:37 +0200

    fix indentation

--- src/common/discord-commands.js
+++ src/common/discord-commands.js
@@ -1,31 +1,31 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
 const logger = require('./logger');
 
 const appId = process.env.DISCORD_APP_ID;
 const botToken = process.env.DISCORD_BOT_TOKEN;
 
 const commands = [
-  {
-    type: 1,
-    name: 'meow',
-    description: 'Meow ?',
-  },
+    {
+        type: 1,
+        name: 'meow',
+        description: 'Meow ?',
+    },
 ];
 
-const rest = new REST({ version: '9' }).setToken(botToken);
+const rest = new REST({version: '9'}).setToken(botToken);
 
 (async () => {
-  try {
-    logger.info('Started refreshing application (/) commands.');
+    try {
+        logger.info('Started refreshing application (/) commands.');
 
-    await rest.put(
-      Routes.applicationCommands(appId),
-      { body: commands },
-    );
+        await rest.put(
+            Routes.applicationCommands(appId),
+            {body: commands},
+        );
 
-    logger.info('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    logger.error(error);
-  }
+        logger.info('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        logger.error(error);
+    }
 })();
--- src/common/logger.js
+++ src/common/logger.js
@@ -1,35 +1,35 @@
 const fontColors = {
-  reset: '\x1b[0m',
-  red: '\x1b[31m',
-  green: '\x1b[32m',
-  yellow: '\x1b[33m',
-  blue: '\x1b[34m',
+    reset: '\x1b[0m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
 };
 
 function getFormattedDateTime() {
-  return new Date().toISOString();
+    return new Date().toISOString();
 }
 
 function error(s) {
-  console.error(`${getFormattedDateTime()}\t${fontColors.red}ERROR${fontColors.reset}\t${s}`);
+    console.error(`${getFormattedDateTime()}\t${fontColors.red}ERROR${fontColors.reset}\t${s}`);
 }
 
 function info(s) {
-  console.info(`${getFormattedDateTime()}\t${fontColors.green}INFO${fontColors.reset}\t${s}`);
+    console.info(`${getFormattedDateTime()}\t${fontColors.green}INFO${fontColors.reset}\t${s}`);
 }
 
 function log(s) {
-  console.log(`${getFormattedDateTime()}\t${fontColors.blue}LOG${fontColors.reset}\t${s}`);
+    console.log(`${getFormattedDateTime()}\t${fontColors.blue}LOG${fontColors.reset}\t${s}`);
 }
 
 function warn(s) {
-  console.warn(`${getFormattedDateTime()}\t${fontColors.yellow}WARN${fontColors.reset}\t${s}`);
+    console.warn(`${getFormattedDateTime()}\t${fontColors.yellow}WARN${fontColors.reset}\t${s}`);
 }
 
 function debug(s) {
-  console.debug(`${getFormattedDateTime()}\t${fontColors.blue}DEBUG${fontColors.reset}\t${s}`);
+    console.debug(`${getFormattedDateTime()}\t${fontColors.blue}DEBUG${fontColors.reset}\t${s}`);
 }
 
 module.exports = {
-  error, info, log, warn, debug,
+    error, info, log, warn, debug,
 };
